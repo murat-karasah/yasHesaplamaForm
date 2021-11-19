50daba3fbb42a2373985ab41f7036a9e12ff0b32
Murat Karasah <mkarasah@gmail.com>
2021-11-19 14:21:01 +0300

    the end

--- yasHesaplamaForm/Form1.cs
+++ yasHesaplamaForm/Form1.cs
@@ -28,9 +28,14 @@ namespace yasHesaplamaForm
            
 
 
-                int ay = Convert.ToInt32(dateTimePicker1.Value.Month);
+            int ay = Convert.ToInt32(dateTimePicker1.Value.Month);
             int ayhesap = Convert.ToInt32(nowtime.Month) - ay;
             int son = 12- Convert.ToInt32(nowtime.Month) + ay;
+
+            if (son >=12 )
+            {
+                son = 1;
+            }
             
             label3.Text = "Yaşınız : "+ sonuc.ToString()+",\n"+ son + " ay sonra "+ sonuc+ " yaşınız dolacak \n ve " + (++sonuc) + " yaşında  olacaksınız";
 
